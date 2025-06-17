edaf8bfc5cef6a077ca84d262e74a39a01b4bfa2
Vikash Gupta <vikash9c35@gmail.com>
2025-06-17 15:50:00 +0530

    Lol

---
+++ CodeWithHarry(C)/CH10/file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    FILE *ptr;
+    ptr = fopen("CodeWithHarry(C)/CH10/vikash.txt", "r");
+    char c;
+    fscanf(ptr, "%c", &c);
+    printf("The character is %c\n", c);
+}
