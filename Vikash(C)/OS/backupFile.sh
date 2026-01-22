633285b6a8647d9cb7752c874167a80026e90b7e
Vikash Gupta <cs25.vikashkumar.gupta@stcet.ac.in>
2026-01-22 06:56:13 +0000

    Back Up File Code

---
+++ Vikash(C)/OS/backupFile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter Your File Name To Generate a backup: "
+read file
+
+if [ -f "$file" ]
+then 
+cp "$file" "$file.bak"
+echo "BackUp File Done"
+else
+echo "Backup Failed 404!!"
+fi
