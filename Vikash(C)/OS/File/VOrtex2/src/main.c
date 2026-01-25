b7e473d980d0c6db0b7a716c7660f7b36a6efdc6
Vikash Gupta <cs25.vikashkumar.gupta@stcet.ac.in>
2026-01-25 15:06:52 +0000

    level5 done

---
+++ Vikash(C)/OS/File/VOrtex2/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Hello from VOrtex2!\n");
+    greet();
+    return 0;
+}
---
+++ Vikash(C)/OS/File/VOrtex2/src/utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "utils.h"
+
+void greet() {
+    printf("Greetings from utils!\n");
+}
